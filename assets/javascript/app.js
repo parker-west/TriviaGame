fbb52e6a40a538bda9a93085fc4b756b97db1b0c
parker-west <mail.parker@gmail.com>
2017-03-23 20:10:09 -0500

    timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,56 @@
-document.ready({ 
+// Initialize JQuery after DOM finishes.
+$(document).ready(function() {
+//File linked?
+console.log("I'm linked.");
+
+
 
 //ON CLICK: Hide START button div on click and show #quiz.
 
 //TIMER LOGIC:
-	//VARIABLE: intervalId will hold setInterval which runs the timer.
-	var = intervalId;
 	//VARIABLE: Set starting time.
-	var time = 10;
-	
+	var number = 10;
+
+	//VARIABLE: intervalId will hold setInterval which runs the timer.
+	var intervalId;
+
+
+	//When the "start" button is clicked, execute the "start" function.
+	$("#start-button").on("click", run);
+
 	//FUNCTION: Start timer and set second interval.
-	function start() {
-		intervalId = setInterval(count, 1000);
+	function run() {
+		intervalId = setInterval(decrement, 1000);
 	}
 
 	// FUNCTION: Decrement timer.
-	function count() {
-		time--;
+	function decrement() {
+		number--;
+	
+		//Show the number in the span of the "timer" div.
+		$("#timer > p > span").html(number);
 	}
 
 
-// End of document.ready		
-});
+	
 
-// video on timer: https://www.youtube.com/watch?v=EGhF4iJSnl0
+	//If timer reaches zero...
+
+		// ...run the "stop" function...
+
+		// ...hide "quiz" div...
+
+		// ...show "results div"
+
+//QUIZ LOGIC:
+
+
+
+// End of document.ready		
+
+
+//Still working?
+console.log('still working.');
+
+//End of doc ready.
+});
